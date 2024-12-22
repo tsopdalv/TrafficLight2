377c7f30738b2ddb1dea8c020e7c6bdd75f73f24
post <postwear25@gmail.com>
2024-12-22 09:15:53 +0300

    Initial Commit

---
+++ TrafficLight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight2
+//
+//  Created by Vladislav Kuznetsov on 22.12.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
