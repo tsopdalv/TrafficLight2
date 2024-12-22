1284ce779a941e356f214c9a51d5657e60584a16
post <postwear25@gmail.com>
2024-12-23 02:46:03 +0300

    TrafficLight with static constraits and outletCollection

--- TrafficLight2/ViewController.swift
+++ TrafficLight2/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+  
+    @IBOutlet var labels: [UIView]!
+    private var currentIndex = 0
+    
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labels.forEach { $0.layer.cornerRadius = $0.frame.width / 2 }
+        
+        startButton.layer.cornerRadius = 10
+
     }
-
-
+    
+    
+    @IBAction func startButtonAction() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        labels.forEach { $0.alpha = 0.3 }
+        labels[currentIndex].alpha = 1.0
+        currentIndex = (currentIndex + 1) % labels.count
+    }
 }
+
+
+
+
 
